e6b80b17aa0a3bf753a464bf895f8292f6060ebc
Mullagaliev <mullagaliev.gagarin@yandex.ru>
2017-08-26 19:16:22 +0300

    Added login form

--- frontend_adhack/src/App.js
+++ frontend_adhack/src/App.js
@@ -17,10 +17,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-
-          To get started, edit <code>src/App.js</code> and save to reload.
+            <Login />
+            To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-          <Test />
       </div>
     );
   }
