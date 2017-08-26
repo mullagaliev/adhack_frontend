0e3143d27bf3f3bbe4a510bfdbd1a767e8ba71b5
Mullagaliev <mullagaliev.gagarin@yandex.ru>
2017-08-26 19:13:12 +0300

    init semantic ui

--- frontend_adhack/src/App.js
+++ frontend_adhack/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Login from './Layouts/LoginLayout';
+import Test from './Layouts/Test';
+
+import { Button, Icon, Grid, Message } from 'semantic-ui-react'
+
+
 class App extends Component {
   render() {
     return (
@@ -11,8 +17,10 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
+
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <Test />
       </div>
     );
   }
--- frontend_adhack/src/index.js
+++ frontend_adhack/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './semantic/dist/semantic.min.css';
+
+import $ from 'jquery';
+import jQuery from 'jquery';
+window.jQuery = jQuery;
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
