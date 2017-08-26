83659ae465dc9c0c0bc526e729dcb7cb7488a260
Mullagaliev <mullagaliev.gagarin@yandex.ru>
2017-08-26 20:55:22 +0300

    added routing

---
+++ frontend_adhack/src/Home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Login from './Layouts/LoginLayout';
+import Test from './Layouts/Test';
+
+import { Button, Icon, Grid, Message } from 'semantic-ui-react'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                    <Icon disabled name='users' />
+                </p>
+                <Login />
+                <Icon.Group size='huge'>
+                    <Icon size='big' color='red' name='dont' />
+                    <Icon color='black' name='user' />
+                </Icon.Group>
+                <Icon.Group size='huge'>
+                    <Icon loading size='big' name='sun' />
+                    <Icon name='user' />
+                </Icon.Group>
+
+            </div>
+        );
+    }
+}
+
+export default App;
--- frontend_adhack/src/index.js
+++ frontend_adhack/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './Home';
+
 import registerServiceWorker from './registerServiceWorker';
 import './semantic/dist/semantic.min.css';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import $ from 'jquery';
 import jQuery from 'jquery';
 window.jQuery = jQuery;
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( ( <BrowserRouter>
+                        <Route path='/' component={App}/>
+                    </BrowserRouter>), document.getElementById('root'));
 registerServiceWorker();
+
