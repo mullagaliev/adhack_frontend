42e8b58cc4fb3168d51f309bc1949a36cf8e5bb1
Mullagaliev <mullagaliev.gagarin@yandex.ru>
2017-08-26 19:20:44 +0300

    fix DOM position Login form

--- frontend_adhack/src/App.js
+++ frontend_adhack/src/App.js
@@ -17,9 +17,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-            <Login />
             To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <Login />
       </div>
     );
   }
